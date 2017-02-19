49c8b46917e69f0fbbaa41f51c57b0fec083019a
Patrikoh <patrik.olsson@axakon.se>
2017-02-19 15:15:11 +0100

    Added stuff

---
+++ Assets/Scripts/MoveRandomly.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MoveRandomly : MonoBehaviour
+{
+    private float timeToChangeDirection;
+    private bool isMoving = true;
+    private Vector3 movingDirection;
+    public float speed;
+    public Animator anim;
+
+    // Use this for initialization
+    public void Start()
+    {
+        InvokeRepeating("ChangeDirection", 0f, Random.Range(2f,5f));
+        anim = GetComponent<Animator>();
+    
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {     
+        if (isMoving)
+        {
+            transform.Translate(-Vector3.forward * speed);
+            anim.Play("Walk", 0);
+        }
+    }
+
+    public void ChangeDirection()
+    {
+
+        if (Random.value > 0.5f)
+        {
+            isMoving = !isMoving;
+        }
+        if (isMoving)
+        {
+            transform.Rotate(Vector3.up, Random.Range(1f, 360f));
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public CharacterController controller;
+    public float speed = 6.0F;
+    public float rotationSpeed = 9000f;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private bool isJumping;
+    public Animator anim;
+    private Vector3 moveDirection = Vector3.zero;
+    private float inputH;
+    private float inputV;
+    private float animationSpeed;
+
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (controller.isGrounded)
+        {
+            anim.SetBool("jump", false);
+            moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+            if (moveDirection != Vector3.zero)
+                transform.rotation = Quaternion.LookRotation(moveDirection);
+
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+                anim.SetBool("jump", true);
+            }
+           
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime * speed);
+
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+
+
+
+        anim.SetFloat("inputH", inputH);
+        anim.SetFloat("inputV", inputV);
+  
+
+
+    }
+}
